1285db397777f64d2a75138dafe909c4275761ca
lekha0204 <kslekha02@gmail.com>
2025-10-15 10:08:15 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const {MongoClient} = require("mongodb");
 const path = require("path");
 
 const app = express();
+dotenv.config();
+MongoClient.connect(process.env.MONGODB_URI)
+  .then(() =>{
+    console.log("Connected to MongoDB successfully")})
+  .catch((err) =>{console.log("Error connecting to MongoDB", err)
+  })
 const PORT = 3000;
+
+console.log(process.env)
 
 // Serve static files directly from project root
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "assets")));
 
 // Route for home page
 app.get("/", (req, res) => {
